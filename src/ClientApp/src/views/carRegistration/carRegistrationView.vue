59d92b50119752628e33902d15fb9028637249bd
tallesdestefanidesouzavaliatti <talles.dsv@parsus.com>
2025-11-10 15:21:19 -0300

    Enhance thread display in car registration modal with a responsive table layout

--- src/ClientApp/src/views/carRegistration/carRegistrationView.vue
+++ src/ClientApp/src/views/carRegistration/carRegistrationView.vue
@@ -312,7 +312,7 @@ const videoUrl = `${window.location.origin}/videos/car-agent.mp4`;
 
   <!-- Modal de Threads -->
   <div v-if="showThreadsModal" class="modal d-block" tabindex="-1" style="background-color: rgba(0,0,0,0.5);">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title"><i class="bi bi-list-ul"></i> Threads Anteriores</h5>
@@ -322,21 +322,37 @@ const videoUrl = `${window.location.origin}/videos/car-agent.mp4`;
           <div v-if="threads.length === 0" class="text-muted text-center py-4">
             Nenhuma thread encontrada.
           </div>
-          <div v-else class="list-group">
-            <button
-              v-for="thread in threads"
-              :key="thread.id"
-              type="button"
-              class="list-group-item list-group-item-action"
-              :class="{ 'active': thread.id === currentThreadId }"
-              @click="loadThread(thread.id)"
-            >
-              <i class="bi bi-chat-dots me-2"></i>
-              <div class="d-flex flex-column">
-                <span class="fw-semibold">Thread: {{ thread.id.substring(0, 8) }}...</span>
-                <small v-if="thread.firstTruncatedMessage" class="text-muted">{{ thread.firstTruncatedMessage }}</small>
-              </div>
-            </button>
+          <div v-else class="table-responsive">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col" style="width: 40%;">ID da Thread</th>
+                  <th scope="col" style="width: 60%;">Primeira Mensagem</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="thread in threads"
+                  :key="thread.id"
+                  @click="loadThread(thread.id)"
+                  style="cursor: pointer;"
+                  :class="{ 'table-active': thread.id === currentThreadId }"
+                >
+                  <td>
+                    <i class="bi bi-chat-dots me-2"></i>
+                    <code>{{ thread.id }}</code>
+                  </td>
+                  <td>
+                    <span v-if="thread.firstTruncatedMessage && thread.firstTruncatedMessage.trim()" class="text-muted">
+                      {{ thread.firstTruncatedMessage }}
+                    </span>
+                    <span v-else class="text-muted fst-italic">
+                      Sem mensagem
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
         <div class="modal-footer">
