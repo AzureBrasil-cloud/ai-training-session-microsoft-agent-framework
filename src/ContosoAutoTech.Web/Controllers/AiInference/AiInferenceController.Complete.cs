1137a64b6be746f94a7ea22896341506d6db9480
tallesdestefanidesouzavaliatti <talles.dsv@parsus.com>
2025-11-11 16:38:45 -0300

    Implement Complete endpoint in AiInferenceController for chat completion requests

---
+++ src/ContosoAutoTech.Web/Controllers/AiInference/AiInferenceController.Complete.cs
@@ -0,0 +1,18 @@
+using ContosoAutoTech.Application.AiInference;
+using ContosoAutoTech.Application.AiInference.Models.Requests;
+using ContosoAutoTech.Web.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoAutoTech.Web.Controllers.AiInference;
+
+public partial class AiInferenceController
+{
+    [HttpPost("/api/inference/complete")]
+    public async Task<IActionResult> Complete(
+        [FromBody] ChatCompletionRequest request,
+        [FromServices] AiInferenceApplicationService service)
+    {
+        return (await service.CompleteAsync(request)).ToApiResponse();
+    }
+}
+
