4b9bb379ffac0830f6f614de79cafbbbf1332ae2
flpdefaria <afariaflp@gmail.com>
2025-11-14 15:12:09 -0300

    Add new logo of powered by

--- src/ClientApp/src/components/common/AzureBrasilLogo.vue
+++ src/ClientApp/src/components/common/AzureBrasilLogo.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="d-flex align-items-center mt-4" :class="alignmentClass">
-    <img :src="logoAzBr" alt="Logo Azure Brasil" class="azure-logo-hover" style="max-height: 120px; cursor: pointer;" @click="openAzureBrasil">
+    <img :src="logoSrc" alt="Logo Azure Brasil" class="azure-logo-hover" style="max-height: 120px; cursor: pointer;" @click="openAzureBrasil">
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref, onMounted, onBeforeUnmount } from 'vue'
 
 interface Props {
   align?: 'start' | 'center' | 'end'
@@ -26,7 +26,45 @@ const alignmentClass = computed(() => {
   }
 })
 
+const theme = ref('');
+
 const logoAzBr = `${window.location.origin}/images/byAzbr.png`
+const logoAzBrLight = `${window.location.origin}/images/byAzbrLight.png`
+
+const logoSrc = computed(() => {
+  return theme.value === 'dark' ? logoAzBr : logoAzBrLight
+})
+
+let observer: MutationObserver | null = null;
+
+onMounted(() => {
+  const savedTheme = sessionStorage.getItem('theme');
+  if (savedTheme) {
+    theme.value = savedTheme;
+  } else {
+    theme.value = 'dark'; // Default theme
+  }
+  document.documentElement.setAttribute('data-bs-theme', theme.value);
+
+
+  observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.attributeName === 'data-bs-theme') {
+        theme.value = document.documentElement.getAttribute('data-bs-theme')!;
+      }
+    });
+  });
+
+  observer.observe(document.documentElement, {
+    attributes: true
+  });
+});
+
+onBeforeUnmount(() => {
+  if (observer) {
+    observer.disconnect();
+  }
+});
 
 const openAzureBrasil = () => {
   window.open('https://azurebrasil.cloud/', '_blank')
